b567e6a11ec94952366927aad6f183586ee919ce
Sho0226 <s1F102201616@iniad.org>
2024-07-02 22:15:00 +0900

    add: board,cell

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,21 @@
 import styles from './index.module.css';
 
 const Home = () => {
-  return <div className={styles.container} />;
+  return (
+    <div className={styles.container}>
+      <div className={styles.boardstyle}>
+        {board.map((row, y) =>
+          row.map((cell, x) => (
+            <div
+              className={styles.cellstyle}
+              key={`${x}-${y}`}
+              onClick={() => clickHandler(x, y)}
+            />
+          )),
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Home;
