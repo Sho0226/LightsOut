9d8e3df13a9e3d6cb7580e133cd6f053d5907415
Sho0226 <s1F102201616@iniad.org>
2024-07-04 00:27:44 +0900

    add: クリック判定,色判定

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,14 +4,26 @@ import styles from './index.module.css';
 const Home = () => {
   const [turnColor, setTurnColor] = useState(1);
   const [board, setBoard] = useState([
-    [0, 0],
+    [2, 1],
     [0, 0],
   ]);
+
+  // const directions = [
+  //   [1, 0],
+  //   [0, 1],
+  //   [-1, 0],
+  //   [0, -1],
+  // ];
+
   const clickHandler = (x: number, y: number) => {
     const newBoard = structuredClone(board);
-    newBoard[y][x] = turnColor;
+    if (board[y] !== undefined && board[y][x] !== undefined) {
+      newBoard[y][x] = turnColor;
+    }
+
     setTurnColor(3 - turnColor);
     setBoard(newBoard);
+    console.table(newBoard);
   };
 
   return (
@@ -23,7 +35,10 @@ const Home = () => {
               {cell !== 0 && (
                 <div
                   className={styles.colorstyle}
-                  style={{ background: cell === 1 ? '#000' : cell === 2 ? '#fff' : '#241b02' }}
+                  style={{
+                    background:
+                      cell === 1 ? '#8de2ff' : cell === 2 ? 'rgba(141, 226, 255, 0.0)' : '#8de2ff',
+                  }}
                 />
               )}
             </div>
