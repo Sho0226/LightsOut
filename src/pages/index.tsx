e442d0027616ed70415fb604c92153925a639dfe
Sho0226 <s1F102201616@iniad.org>
2024-07-03 12:16:38 +0900

    cell,color

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,18 +7,26 @@ const Home = () => {
     [0, 0],
     [0, 0],
   ]);
-  const clickHandler = (x: number, y: number) => {};
+  const clickHandler = (x: number, y: number) => {
+    const newBoard = structuredClone(board);
+    newBoard[y][x] = turnColor;
+    setTurnColor(3 - turnColor);
+    setBoard(newBoard);
+  };
 
   return (
     <div className={styles.container}>
       <div className={styles.boardstyle}>
         {board.map((row, y) =>
           row.map((cell, x) => (
-            <div
-              className={styles.cellstyle}
-              key={`${x}-${y}`}
-              onClick={() => clickHandler(x, y)}
-            />
+            <div className={styles.cellstyle} key={`${x}-${y}`} onClick={() => clickHandler(x, y)}>
+              {cell !== 0 && (
+                <div
+                  className={styles.colorstyle}
+                  style={{ background: cell === 1 ? '#000' : cell === 2 ? '#fff' : '#241b02' }}
+                />
+              )}
+            </div>
           )),
         )}
       </div>
