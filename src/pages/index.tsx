449acc7de255239e2eaa6d44167f2c1125cee379
Sho0226 <s1F102201616@iniad.org>
2024-07-05 18:11:48 +0900

    fix: 初期色

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,16 +4,9 @@ import styles from './index.module.css';
 const Home = () => {
   const [turnColor, setTurnColor] = useState(1);
   const [board, setBoard] = useState([
-    [2, 1],
+    [1, 1],
     [0, 0],
   ]);
-
-  const directions = [
-    [1, 0],
-    [0, 1],
-    [-1, 0],
-    [0, -1],
-  ];
 
   const clickHandler = (x: number, y: number) => {
     const newBoard = structuredClone(board);
@@ -37,6 +30,8 @@ const Home = () => {
     setBoard(newBoard);
     console.table(newBoard);
   };
+  // const colorNum = board.flat().filter((cell) => cell !== 0).length;
+  // console.log(colorNum);
 
   return (
     <div className={styles.container}>
@@ -44,15 +39,12 @@ const Home = () => {
         {board.map((row, y) =>
           row.map((cell, x) => (
             <div className={styles.cellstyle} key={`${x}-${y}`} onClick={() => clickHandler(x, y)}>
-              {cell !== 0 && (
-                <div
-                  className={styles.colorstyle}
-                  style={{
-                    background:
-                      cell === 1 ? '#8de2ff' : cell === 2 ? 'rgb(221, 227, 233)' : '#8de2ff',
-                  }}
-                />
-              )}
+              <div
+                className={styles.colorstyle}
+                style={{
+                  background: cell === 1 ? '#8de2ff' : 'rgb(221, 227, 233)',
+                }}
+              />
             </div>
           )),
         )}
